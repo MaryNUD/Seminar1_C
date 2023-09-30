360d4163f6a59a58663c9bed29a39959c90ede0d
Mary Nudnova <m.nudnova@yandex.ru>
2023-09-30 12:00:25 +0300

    add Task03 div

---
+++ Task03_div/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Write the number :");
+int n = Convert.ToInt32(Console.ReadLine());
+if ((n % 2) == 0)
+    Console.WriteLine("The number is even");
+else 
+    Console.WriteLine("The number is uneven");
