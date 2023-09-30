ebc8807b0851410972a60f36174f7fefef52f87d
Mary Nudnova <m.nudnova@yandex.ru>
2023-09-30 11:51:49 +0300

    add Task02 max2

---
+++ Task02_max2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Write the number a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Write the number b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Write the number c:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+else if (c > max)
+{
+    max = c; 
+}
+Console.Write("max number = ");
+Console.Write(max);
