7124ad78cfae672b59c39bacf2ac7a58a7fe9f2e
Mary Nudnova <m.nudnova@yandex.ru>
2023-09-30 12:27:08 +0300

    add Task04 array

---
+++ Task04_array/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Write the number:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 1)
+{
+    for (int i = 1; i <= n; i++)
+    if ((i % 2) == 0)
+        Console.Write(i + " ");
+}
+else
+Console.WriteLine("The inserted number is <= 1, error");
