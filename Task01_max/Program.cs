4cd0336e53e9914638f6c1df19488e2764781352
Mary Nudnova <m.nudnova@yandex.ru>
2023-09-30 11:27:11 +0300

    add Task01 max

---
+++ Task01_max/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Write the number a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Write the number b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.Write(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.Write(b);
+}
